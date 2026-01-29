6d45634989f5181b5c6a0ed64af74d3cbeb07e3d
Limochuan <jiemingzhang001@gmail.com>
2026-01-29 15:30:52 +0800

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,13 @@
+import pymysql
+import os
 
+def get_conn():
+    return pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=True
+    )
